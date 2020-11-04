6c521bd3d5c724bc1478d18d5b386e9badcca38f
Greg Albers <galbers@getty.edu>
2020-11-04 15:42:49 -0800

    Add links to load other years

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// https://stackoverflow.com/questions/19844545/replacing-css-file-on-the-fly-and-apply-the-new-style-to-the-page
+function changeCSS(cssFile, cssLinkIndex) {
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink)
+
+}
